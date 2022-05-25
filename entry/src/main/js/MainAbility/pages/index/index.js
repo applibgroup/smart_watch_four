be9c113e17f6ffa99c3de2ae92809bf72afcd0f1
mahesh-11102 <dasikamahesh1@gmail.com>
2022-05-25 22:25:39 +0530

    added copyright details

--- entry/src/main/js/MainAbility/pages/index/index.js
+++ entry/src/main/js/MainAbility/pages/index/index.js
@@ -1,11 +1,26 @@
-// @ts-nocheck
+/*
+* Copyright (C) 2022 Application Library Engineering Group
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import ability_featureAbility from '@ohos.ability.featureAbility';
 import fetch from '@system.fetch';
 import geolocation from '@system.geolocation';
-import json_data from '../../i18n/weather_data.json';
+import sensor from '@system.sensor'
+
 export default {
     data: {
         weather: "sun & windy",
-        // cloudy, windy, partly sunny, rainy, sleeting, sun & rain, sun & windy, sunny, thunderstorm & rain, thunderstorm
         time_h: "09",
         time_m: "30",
         date_w: "Mon",
@@ -13,113 +28,180 @@ export default {
         date_m: "Feb",
         bpm: 120,
         kcal: 1002,
-        time_period:"AM",
-        //PM or AM
+        time_period: "AM",
         step: "10000",
-        noti: "Team Meeting 11am",
+        noti: "",
+        lat: "16.5062",
+        long: "80.6480"
     },
     onInit() {
         this.fetchDate();
         this.fetchTime();
         this.fecthWeather();
+        this.fecthData();
     },
-    fetchDate : function(){
+    fetchDate: function () {
         let date = new Date();
-        const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-        const week = ["Mon", "Tue","Wed","Thu","Fri","Sat","Sun"]
+        const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
         this.date_d = date.getDate();
         this.date_m = month[date.getMonth()];
         this.date_w = week[date.getDay()-1]
     },
 
-    fetchTime : function(){
+    fetchTime: function () {
         let date = new Date();
 
 
         let hrs = date.getHours() + ""
 
-        // 4 7
-        if(hrs <10)
-        {
-            this.time_h_0 = 0 ;
+        if (hrs < 10) {
+            this.time_h_0 = 0;
             this.time_h_1 = hrs[0];
         }
-        else
-        {
+        else {
             this.time_h_0 = hrs[0];
             this.time_h_1 = hrs[1];
         }
 
         let min = date.getMinutes() + ""
 
-        if(min <10)
-        {
+        if (min < 10) {
             this.time_m_0 = 0;
             this.time_m_1 = min[0];
         }
-        else
-        {
+        else {
             this.time_m_0 = min[0];
             this.time_m_1 = min[1];
         }
 
-        //        this.time_m_0 = 9
-        //        this.time_m_1 = 9
-
-        this.time_period = date.getHours()  >= 12 ? 'PM' : 'AM';
+        this.time_period = date.getHours() >= 12 ? 'PM' : 'AM';
 
     },
-
-    fecthWeather : function(){
-
-
-        geolocation.getLocation( (err, data) => {
-            console .log( 'getLastLocation: ' + err + " data: " + JSON .stringify(data));
-        });
-
-
-        var dataw = JSON.stringify(json_data);
-        console.log("JSON Data : "+ JSON.stringify(json_data));
-        let weather_data = JSON.parse(dataw);
+    fecthData: function () {
         let data;
         fetch.fetch({
-            url:'https://api.openweathermap.org/data/2.5/weather?lat='+ weather_data[0].lat +'&lon='+weather_data[0].long+'&appid=9ca3abfc02f621a4fe7696f670f04a57',
-            responseType:"JSON",
+            url: 'https://mocki.io/v1/cb0cd987-0c49-4e7c-9475-fb533723e2fd',
+            responseType: "json",
             method: 'GET',
-            success:function(resp)
-            {
-                data = JSON.parse(resp.data);
-                console.log("Data :" +data + " "+ resp + " ");
-            },
-            fail:(data,code) => {
-                console.log("fail data: "+ JSON.stringify(data) + " fail code: "+ code );
-            },
-            complete: ()=>{
-                this.weather = data.weather[0].main ;
-                console.log("Weather :"+this.weather+ " "+ this.weather[0].main);
 
+            success: function (res) {
+                data = JSON.parse(res.data);
+                console.log("Data :" + data + " " + JSON.stringify(res) + " ");
+            },
+
+            fail: (data, code) => {
+                console.log("Fail Data: noti " + JSON.stringify(data) + " fail code: " + code);
+            },
+            complete: () => {
+                this.bpm = data.bpm;
+                this.kcal = data.cal;
+                this.noti = data.noti;
+                console.log(this.bpm + " " + this.kcal + " " + this.noti);
             }
         })
-
-
     },
-    onClickWeatherContainer : function() {
+
+    fecthWeather: function () {
+        let locData;
+        geolocation.getLocation({
+            success: function (data) {
+                locData = data;
+                console.log("The location fetched:" + JSON.stringify(data));
+            },
+            fail: function (data, code) {
+                console.log('fail to get location. code:' + code + ', data:' + data);
+            },
+            complete: function () {
+                console.log('in Complete');
+            }
+        })
+        console.log('Location Data: ' + JSON.stringify(locData) + " lat: " + locData.latitude + " long: " + locData.longitude);
+
+        this.lat = locData.latitude;
+        this.long = locData.longitude;
+
+        console.log("Lat : " + this.lat + " Long : " + this.long);
+
+        let data;
+        fetch.fetch({
+            url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + this.lat + '&lon=' + this.long + '&appid=9ca3abfc02f621a4fe7696f670f04a57',
+            responseType: "JSON",
+            method: 'GET',
+            success: function (resp) {
+                data = JSON.parse(resp.data);
+                console.log("Data :" + data + " " + resp + " ");
+            },
+            fail: (data, code) => {
+                console.log("fail data: " + JSON.stringify(data) + " fail code: " + code);
+            },
+            complete: () => {
+                this.weather = data.weather[0].main;
+                console.log("Weather :" + this.weather + " " + this.weather[0].main);
+            }
+        })
+    },
+    onClickWeatherContainer: function () {
         console.log("Weather container");
     },
-    onClickDateContainer : function() {
+    onClickDateContainer: function () {
         console.log("Date container");
     },
-    onClickCaloriesContainer : function() {
+    onClickCaloriesContainer: function () {
         console.log("Calories container");
     },
-    onClickBPMContainer : function() {
+    onClickBPMContainer: function () {
         console.log("BPM container");
     },
-    onClickStepsContainer : function() {
+    onClickStepsContainer: function () {
         console.log("Steps container ");
-    }
+    },
+    async startCounting() {
+        if (!this.sensorPermission) {
+            this.sensorPermission = await verifyPermissions()
+        } else {
+            subscribePedometerSensor(this)
+        }
+    },
+    async fetchLocation() {
 
+        geolocation.isLocationEnabled((err, data) => {
+            console.log('isLocationEnabled: ' + err + " data: " + data);
+        });
+
+        var requestInfo = {
+            'priority': 0x203, 'scenario': 0x300, 'maxAccuracy': 0
+        };
+        geolocation.getCurrentLocation(requestInfo).then((result) => {
+            console.log('current location: ' + JSON.stringify(result.data));
+        });
+    }
+}
+
+function subscribePedometerSensor(context) {
+    sensor.subscribeStepCounter({
+        success: function (ret) {
+            context.step = ret.steps.toString()
+        },
+        fail: function (data, code) {
+            console.log('Subscription failed. Code: ' + code + '; Data: ' + data)
+        }
+    })
+}
+
+function verifyPermissions() {
+    var context = ability_featureAbility.getContext()
+    let permission = "ohos.permission.ACTIVITY_MOTION"
+    var result = new Promise((resolve, reject) => {
+        context.verifyPermission(permission)
+            .then((data) => {
+                resolve(true)
+            }).catch((error) => {
+            reject(false)
+        })
+    })
+    return result
 }
 
 // Weather API  https://api.openweathermap.org/data/2.5/weather?lat=16.5062&lon=80.6480&appid=9ca3abfc02f621a4fe7696f670f04a57
